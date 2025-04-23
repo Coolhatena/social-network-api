1f35e2112958f5312b189b0fbe05fc38259e5ae8
Coolhatena <ingsc.geffroy@gmail.com>
2025-04-23 16:53:55 -0700

    Create express server

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-const connection = require("./database/connection")
+const connection = require("./database/connection");
+const express = require("express");
+const cors = require('cors');
 
 // DB connection
 connection();
+
+// Create node server
+const app = express();
+const port = 3900;
+
+// Configure CORS
+app.use(cors());
