d412c7efd821b0514e5d76c43ce54db9d06efdd1
Coolhatena <ingsc.geffroy@gmail.com>
2025-04-26 16:00:42 -0700

    Create user register route

--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,6 @@ const userController = require("../controllers/user");
 const router = express.Router();
 
 router.get("/user-test", userController.testUser);
+router.get("/register", userController.register)
 
 module.exports = router;
