a219ecc9cd3124b10ca3fa2768f6f0fabf9e7193
Coolhatena <ingsc.geffroy@gmail.com>
2025-04-27 07:53:54 -0700

    NIT: Remove first person from comment

--- run-api.sh
+++ run-api.sh
@@ -1,2 +1,2 @@
-xfce4-terminal -T "Mongo Server" -e "bash -ic 'mongoserver'" # "mongoserver" is an alias created by me for "mongod --dbpath=/path/to/dbs"
+xfce4-terminal -T "Mongo Server" -e "bash -ic 'mongoserver'" # "mongoserver" is an alias for "mongod --dbpath=/path/to/dbs"
 xfce4-terminal -T "API" -e "npm start"
