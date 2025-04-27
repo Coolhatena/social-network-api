1300b67f259965f9563850ed571e1b5abaab6822
Coolhatena <ingsc.geffroy@gmail.com>
2025-04-27 08:24:33 -0700

    Add validation for user regiter

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,5 @@
+const User = require("../models/user");
+
 // Mock action
 const testUser = (req, res) => {
 	return res.status(200).send({
@@ -7,9 +9,45 @@ const testUser = (req, res) => {
 
 // Register
 const register = (req, res) => {
-	return res.status(200).json({
-		message: "Action - Register user"
-	})
+	const params = req.body;
+
+	if (!params.name || !params.nick || !params.email || !params.password) {
+		console.log("FAILED VALIDATION")
+		return res.status(400).json({
+			status: "error",
+			message: "Incomplete data"
+		})
+	}
+
+	const userCached = new User(params);
+	try {
+		const duplicatedUser = User.find({
+			$or: [
+				{ email: userCached.email.toLowerCase()},
+				{ nick: userCached.nick.toLowerCase()}
+			]
+		}).exec()
+
+		if (duplicatedUser && duplicatedUser.length >= 1){
+			return res.status(400).json({
+				status: "error",
+				message: "This user already",
+			})
+		}
+
+
+
+		return res.status(200).json({
+			status: "success",
+			message: "Action - Register user",
+			userCached
+		})
+	} catch (err) {
+		return res.status(500).json({
+			status: "error",
+			message: "Internal server error",
+		})
+	}
 }
 
 module.exports = {
