fb750b30da74b5c6b6bbe8ba054d6c17b33453b6
Coolhatena <ingsc.geffroy@gmail.com>
2025-04-28 23:29:51 -0700

    NIT: Add semicolon

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,6 @@ const userController = require("../controllers/user");
 const router = express.Router();
 
 router.get("/user-test", userController.testUser);
-router.post("/register", userController.register)
+router.post("/register", userController.register);
 
 module.exports = router;
