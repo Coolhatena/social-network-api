854224aea39f96e19a82144777e1a7b5e6a58776
Coolhatena <ingsc.geffroy@gmail.com>
2025-05-04 13:11:36 -0700

    Add middleware to validate auth tokens

---
+++ middlewares/auth.js
@@ -0,0 +1,35 @@
+const jwt = require('jwt-simple');
+const moment = require('moment');
+const libjwt = require('../services/jwt');
+
+const secret = libjwt.secret;
+
+// Auth middleware
+exports.auth = (req, res, next) => {
+	if (!req.headers.authorization) {
+		return res.status(403).send({
+			status: "error",
+			message: "Authorization header missing in request",
+		});
+	}
+
+	let token = req.req.headers.authorization.replace(/['"]+/, '');
+	try {
+		let payload = jwt.decode(token, secret);
+		if (payload.exp <= moment().unix()) {
+			return res.status(401).send({
+				status: "error",
+				message: 'Expired token',
+			});
+		}
+	} catch (error) {
+		return res.status(404).send({
+			status: "error",
+			message: "Invalid token",
+		});
+	}
+
+	req.user = payload;
+
+	next();
+}
