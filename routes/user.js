289eaae24d53af96c0fb8d2f944ec70695545972
Coolhatena <ingsc.geffroy@gmail.com>
2025-05-04 13:17:21 -0700

    Add token validation middleware to test-route

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const userController = require("../controllers/user");
+const { auth } = require('../middlewares/auth');
 
 const router = express.Router();
 
-router.get("/user-test", userController.testUser);
+router.get("/user-test", auth, userController.testUser);
 router.post("/register", userController.register);
 router.post("/login", userController.login);
 
