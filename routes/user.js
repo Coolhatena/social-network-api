a85a0b7e7572592d38d3ad461be798aaf0582e29
Coolhatena <ingsc.geffroy@gmail.com>
2025-05-05 19:53:03 -0700

    Create profile route

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,7 @@ const { auth } = require('../middlewares/auth');
 const router = express.Router();
 
 router.get("/user-test", auth, userController.testUser);
+router.get("/profile/:id", auth, userController.getProfile)
 router.post("/register", userController.register);
 router.post("/login", userController.login);
 
