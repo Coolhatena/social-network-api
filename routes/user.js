5ae0545a959fc91d20ba1d9421fcb0d096d6203b
Coolhatena <ingsc.geffroy@gmail.com>
2025-05-06 23:03:41 -0700

    Create list users route

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.get("/user-test", auth, userController.testUser);
 router.get("/profile/:id", auth, userController.getProfile);
+router.get("/list/:page?", auth, userController.listUsers);
 router.post("/register", userController.register);
 router.post("/login", userController.login);
 
