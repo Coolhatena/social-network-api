1d553ab9863eadd92a4b7a315a420d9d3f385b51
Eduardo Rivera Geffroy <94434891+Coolhatena@users.noreply.github.com>
2025-06-14 23:29:16 -0700

    Add TODO

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const { auth } = require('../middlewares/auth');
 
 const router = express.Router();
 
+// TODO: Change different routes for login/register to same route different method 
 router.get("/user-test", auth, userController.testUser);
 router.get("/profile/:id", auth, userController.getProfile);
 router.get("/list/:page", auth, userController.listUsers);
